a07e52e80c25543dd8f4d1997c4342826e7e2231
Miguel <miguelespigares@gmail.com>
2019-03-05 12:22:36 +0100

    first commit

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Vamos a operarrferrr");
+        System.out.println("Vamos a operarrferrrr");
     }
 
 }
